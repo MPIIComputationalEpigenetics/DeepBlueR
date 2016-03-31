3c4ba1c522024af5e70e72315731cc577209f429
Nadia-Ashraf <nadia.ashraf98@gmail.com>
2016-03-31 16:11:20 +0200

    documentation fixed and get_value removed

--- R/use_case1.R
+++ R/use_case1.R
@@ -3,7 +3,7 @@
 # Author: Nadia Ashraf
 
 source("deepblue.R")
-source("helpingFunctions.R")
+source("D:/HiWi/DeepBlue-R/py/helpingFunctions.R")
 
 
 #result of functions is a 2 elements list. Extract 2nd element convert it to string,
@@ -15,23 +15,21 @@ source("helpingFunctions.R")
 
 #deepblue.select_regions takes 11 parameters as input, namely; eperiment_name,genome,epigentic_
 #mark,sample_id,technique,project,chromosome,start,end, and user_key. All parameters have default
-#NULL value which can be changed according to requirement. user_id is set to anonymous_key which
+#NULL value which can be changed according to requirement. user_key is set to anonymous_key which
 #can also be changed with your specific user key.
 
 
 sel_regions = deepblue.select_regions(genome ='GRCh38',epigenetic_mark ='H3k27ac',
-                                      project ='BLUEPRINT Epigenome',chromosome ='chr1')
+                                      project ='BLUEPRINT Epigenome',chromosome ='chr1')[[2]]
 
 #Extract peaks for H3k27ac
 
 
 #deepblue.query_experiment_type takes 3 input parameters, namely; query_id, type, and user_key.
-#All values are set to NULL as default.
+#user_key is set to anonymous_key by default
 
-
-val_regions = get_value(sel_regions)
-sel_regions_peaks=deepblue.query_experiment_type(query_id =val_regions,
-                                                 type ='peaks')
+sel_regions_peaks=deepblue.query_experiment_type(query_id =sel_regions,
+                                                 type ='peaks')[[2]]
 
 #Extract annotated promoter regions from GRCh38 genome assembly
 
@@ -41,59 +39,57 @@ sel_regions_peaks=deepblue.query_experiment_type(query_id =val_regions,
 
 
 promoters = deepblue.select_annotations(annotation_name ='promoters',genome ='GRCh38',
-                                        chromosome ='chr1')
+                                        chromosome ='chr1')[[2]]
 
 #filter for promoter regions having H3k27ac modification associated with them
 
 
 #deepblue.intersection takes 3 parameters as input, namely; query_a_id, query_b_id, and user_key.
-#All parameters are set to NULL as default.
+#user_key is set to anonymous_key which can also be changed with your specific user key.
 
 
-val_peaks = get_value(sel_regions_peaks)
-val_prom = get_value(promoters)
-sel_promoters = deepblue.intersection(query_a_id = val_peaks,
-                                      query_b_id = val_prom)
+
+sel_promoters = deepblue.intersection(query_a_id = sel_regions_peaks,
+                                      query_b_id = promoters)[[2]]
 
 #Extract transcription factors AG01,AG02,and AG03 from ENCODE project
 
 tf = deepblue.select_regions(genome ='hg19',epigenetic_mark ='SP1',
-                             project ='ENCODE',chromosome ='chr1')
+                             project ='ENCODE',chromosome ='chr1')[[2]]
 
 #Extract signal for transcription factors
 
-qid_tf =  get_value(tf)
-sel_tf_signal = deepblue.query_experiment_type(query_id =qid_tf,type ='signal')
+
+sel_tf_signal = deepblue.query_experiment_type(query_id =tf,type ='signal')[[2]]
 
 #Extract TFs overlaping with sel_promoters
 
-qid_prom = get_value(sel_promoters)
-sel_tf = deepblue.intersection(query_a_id =qid_tf,query_b_id =qid_prom)
+
+sel_tf = deepblue.intersection(query_a_id =tf,query_b_id =sel_promoters)[[2]]
 
 #Get chromosome regions
 
 
 #deepblue.get_regions expects 3 input parameters; query_id, output_format, and user_key."
 
-id_sel_tf = get_value(sel_tf)
-req_regions = deepblue.get_regions(query_id = id_sel_tf,output_format =
-                                       "CHROMOSOME,START,END,@NAME,@EPIGENETIC_MARK,@BIOSOURCE")
+
+req_regions = deepblue.get_regions(query_id =sel_tf,output_format =
+                                       "CHROMOSOME,START,END,@NAME,@EPIGENETIC_MARK,@BIOSOURCE")[[2]]
 
 
 #Process request
 
-id_req_regions = get_value(req_regions)
-info = deepblue.info(id_req_regions)
+info = deepblue.info(req_regions)
 
 
 #process.request expects 3 input parameters; requested_regions, sleep.time, user_key. sleep.time
 #is set to 1s as default sleeping time.
 
 request_info = process_request(requested_regions = req_regions)
-if (request_info[[2]]$value$state == "done") {
+if (request_info$value$state == "done") {
     print("The processing was finished")
 } else {
-    stop(request_info[[2]]$value$message)
+    stop(request_info$value$message)
 }
 
 #get regions that contains the TFs that overlap with the H3K27ac and the promoters regions.
--- py/helpingFunctions.R
+++ py/helpingFunctions.R
@@ -1,35 +1,32 @@
 suppressMessages(library(GenomicRanges))
 
-#'process_request
-#'
-#'Process the user request. Takes in three parameters; requested regions, sleep time, and user key.
+#'@title process_request
+#'@description Process the user request. Takes in three parameters; requested regions, sleep time, and user key.
 #'@param requested_regions A string 
 #'@param sleep.time An integer with default value 1s
 #'@param user_key A string 
 
 process_request = function (requested_regions,sleep.time = 1, user_key=deepblue.USER_KEY)
 {
-  info = deepblue.info(as.character(requested_regions[2]), user_key)
+  info = deepblue.info(requested_regions, user_key)[[2]]
   
-  state = info[[2]]$value$state
+  state = info$value$state
   while (state != 'done' & state != 'error')
   {
     Sys.sleep(sleep.time)
-    info = deepblue.info(as.character(requested_regions[2]), user_key)
-    state = info[[2]]$value$state
+    info = deepblue.info(requested_regions, user_key)[[2]]
+    state = info$value$state
   }
   info
 }
 
-#'convert_to_df
-#'save output in a data frame for further processing.Expects two parameters; the output string from 
-#'method deepblue.get_request_data and request information from method process_request.
-#'
+#'@title convert_to_df
+#'@description save output in a data frame for further processing.Expects two parameters; the output string from method deepblue.get_request_data and request information from method process_request.
 #' @param output A string 
 #' @param inf A list with request information
 #' @return regions A data frame  
 
-convert_to_df_fast = function(output, inf, dict=col_dict){
+convert_to_df = function(output, inf, dict=col_dict){
 
     #dependencies used here
     library(data.table)
@@ -45,6 +42,7 @@ convert_to_df_fast = function(output, inf, dict=col_dict){
         if(is.null(col_type)) return("character")
         #need to change string to character
         else if(col_type == "string") return("character")
+        else if (col_type == 'category') return ('factor')
         else return(col_type)
     })
     
@@ -59,64 +57,8 @@ convert_to_df_fast = function(output, inf, dict=col_dict){
           data.table = FALSE)
 }
 
-convert_to_df = function(output=NULL,inf=NULL)
-{
-  final = unlist(strsplit(as.character(output),'\n'))
-  final = as.data.frame(final, stringsAsFactors=FALSE)
-  regions = data.frame(do.call('rbind', strsplit(as.character(final$final),'\t',fixed=TRUE)),stringsAsFactors=FALSE)
-  colnames(regions) = unlist(strsplit(inf$value$value$format,','))
-  regions = convert_type(regions,col_dict)
-  return (regions)
-}
-
-#'convert_type
-#'Sets data types of each column in the data frame. Expects two input; A dataframe and a dictionary.
-#'
-#'@param df A dataframe
-#'@param dict A dictionary with column names as keys and column types as values.  
-#'@return df A dataframe
-
-convert_type = function(df=NULL,dict)
-{
-  stopifnot(is.list(df))
-  cols = colnames(df)
-  types = c()
-  for (i in cols)
-  {
-    types=c(types,dict[[i]])
-  }
-  for (i in 1:length(types))
-  {
-    if (types[i] == "integer") 
-    { 
-      df[i] <- as.integer(df[[i]])
-    }
-    else if (types[i] == "character")
-    {
-      df[i] <- as.character(df[[i]])
-    }
-    else if (types[i] == "double")
-    {
-      df[i] <- as.double(df[[i]])
-    }
-    else 
-    {
-      df[i] <- as.factor(df[[i]])
-    }
-  }
-  return(df)
-}
-
-#extract value from list
-
-get_value = function (input = NULL)
-{
-  value = as.character(input[2])
-  return (value)
-}
-
-#'convert_to_grange
-#'Converts the requested data into GRanges object. Expects one input; A dataframe with requested data.
+#'@title convert_to_grange
+#'@description Converts the requested data into GRanges object. Expects one input; A dataframe with requested data.
 #'
 #'@param df A data frame
 #'@return region_gr A GRanges object
@@ -131,8 +73,8 @@ convert_to_grange = function (df = NULL)
   return (region_gr)
 }
 
-#'get_request_data
-#'Returns the requested data as GRanges object. Expects two input parameters; Request information and
+#'@title get_request_data
+#'@description Returns the requested data as GRanges object. Expects two input parameters; Request information and
 #'user key. It depends on outputs from several functions, namely;
 #'deepblue.get_request_data, convert_to_df, and convert_to_grange.
 #'
@@ -143,12 +85,12 @@ convert_to_grange = function (df = NULL)
 #'@seealso \code{\link{deepblue.get_request_data}}, \code{\link{convert_to_df}}, and
 #'\code{\link{convert_to_grange}}.
 
-#//TODO: REMOVE??
-get_request_data = function (request_info=NULL, user=deepblue.USER_KEY, type="grange")
+
+get_request_data = function (request_info, user=deepblue.USER_KEY, type="grange")
 {
     request_id = request_info[[2]]$value$`_id`
     final_regions = deepblue.get_request_data(request_id = request_id, user_key = user)
-    regions = convert_to_df_fast(output=final_regions[[2]][[1]], inf=request_info[[2]][[1]])
+    regions = convert_to_df(output=final_regions[[2]][[1]], inf=request_info[[2]][[1]])
     
     if(type == "grange") return(convert_to_grange(df=regions))
     else return (regions)
@@ -166,16 +108,14 @@ get_columns = function()
   
   col_names=c()
   col_types = c()
-  
-  for (i in col_ids)
+  col_info = deepblue.info(col_ids)
+  for (i in 1:length(col_info[[2]]))
   {
-    inf = deepblue.info(i)
-    col_names=c(col_names,inf[[2]]$value$name)
-    col_types=c(col_types,inf[[2]]$value$column_type)
+    col_names=c(col_names,col_info[[2]][i]$value$name)
+    col_types=c(col_types,col_info[[2]][i]$value$column_type)
   }
   
   col_info=data.frame(col_names,col_types,stringsAsFactors = FALSE)
-  col_info[col_info=='category']='factor'
   dict=new.env()
   for(i in seq(nrow(col_info)))
   {
