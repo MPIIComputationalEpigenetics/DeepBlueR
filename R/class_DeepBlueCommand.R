3ae56a5346f17701093a7f7b3a5357a204d0e255
mlist <mlist@mpi-inf.mpg.de>
2016-04-14 14:05:33 +0200

    export DeepBlueCommand class was missing

--- R/class_DeepBlueCommand.R
+++ R/class_DeepBlueCommand.R
@@ -1,3 +1,14 @@
+#'@title DeepBlueCommand class
+#'@export 
+#'@description An S4 class returned when calling a DeepBlue-R function. It holds information about
+#'the original call, the query / request status, previous commands, the user_key, and results in GRanges format once
+#'a request is downloaded.#'
+#'@param call language
+#'@param status character
+#'@param query_id character
+#'@param previous_commands list
+#'@param user_key character
+#'@param result GRanges 
 DeepBlueCommand <- setClass("DeepBlueCommand",
                             slots = c(call = "language",
                                       status = "character",
