bf31ef28572ee5b9d7b87c95df21fc80187d1e9b
Felipe Albrecht <felipe.albrecht@gmail.com>
2016-04-22 15:54:24 +0200

    Three examples

---
+++ build/examples/deepblue.add_biosource.R
@@ -0,0 +1,1 @@
+deepblue.add_biosource(name="New BioSource", description="This Biosource is used for..", extra_metadata=list(source="Ontology Blabla", reference="http://ontology.blabla/id"), user_key="my_private_user_key")
---
+++ build/examples/deepblue.add_epigenetic_mark.R
@@ -0,0 +1,1 @@
+deepblue.add_epigenetic_mark ( name="H3k123ac Name", description="A brand new histone mark", extra_metadata=list(type="Histone Mark"), user_key="my_secret_key" )
