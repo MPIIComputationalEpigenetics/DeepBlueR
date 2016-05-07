67bb8d975970cee9bfdd0492874614e0c4d755d2
Felipe Albrecht <felipe.albrecht@gmail.com>
2016-05-07 20:38:34 +0200

    Fix aggregate example.

--- build/examples/deepblue.aggregate.R
+++ build/examples/deepblue.aggregate.R
@@ -1,2 +1,6 @@
-deepblue.aggregate(data_id = "q123456", ranges_id = "q654321",
-  column = "SCORE", user_key = deepblue.USER_KEY)
+annotation_id = deepblue.select_annotations(annotation_name="CpG Islands",
+	genome="hg19", chromosome="chr1")
+data_id = deepblue.select_experiments(
+	experiment_name="E002-H3K9ac.narrowPeak.bed")
+deepblue.aggregate(data_id = data_id, ranges_id=annotation_id,
+	column = "SCORE")
